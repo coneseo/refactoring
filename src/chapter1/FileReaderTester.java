3ab8625879f5fa6bdcef84714965589ac703fa61
coneseo <woon4754@gmail.com>
2021-09-22 23:38:40 +0900

    #20210922 Write small test code for FileReaderTester

--- src/chapter1/FileReaderTester.java
+++ src/chapter1/FileReaderTester.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class FileReaderTester {
     protected void setUp(){
         try{
-            input = new FileReader("data.txt");
+            _input = new FileReader("data.txt");
         }catch(FileNotFoundException e){
             throw new RuntimeException("Can not open file");
         }
@@ -15,9 +15,18 @@ public class FileReaderTester {
 
     protected void tearDown(){
         try{
-            input.close();
+            _input.close();
         }catch (IOException e){
             throw new RuntimeException("Error during closing file");
         }
+    }
+
+    public void testRead() throws IOException{
+        char ch = '&';
+        for (int i=0; i < 4; i++){
+            ch = (char) _input.read();
+            assert('d' == ch);
+        }
+
     }
 }
